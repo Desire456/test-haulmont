f29ed341705d9b00ba30c0eb8b3aab39c5142a6f
Yuriy Artamonov <artamonov@haulmont.com>
2014-12-22 14:22:29 +0400

    Update Vaadin version to 7.3.7, use java for UI class

---
+++ src/main/groovy/com/haulmont/vaadin/sample/TestUI.java
@@ -0,0 +1,21 @@
+package com.haulmont.vaadin.sample;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+
+@Theme(ValoTheme.THEME_NAME)
+public class TestUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setMargin(true);
+
+        layout.addComponent(new Label("Vaadin 7 Sample"));
+
+        setContent(layout);
+    }
+}
